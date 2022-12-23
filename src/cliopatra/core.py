de875bc9ed37765f6aa3dad72c26996660becf79
Renato Damas <me@renatodamas.com>
2022-12-22 20:21:12 -0500

    chore: rename project

---
+++ src/cliopatra/core.py
@@ -0,0 +1,24 @@
+import argparse
+
+from pydantic import BaseModel
+
+
+class Cliopatra(argparse.ArgumentParser):
+    # Initialise Super Class
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+
+if __name__ == "__main__":
+
+    def cliopatra():
+        pass
+
+    def pharaoh():
+        pass
+
+    Cliopatra(tree={
+        cliopatra: {
+            pharaoh
+        }
+    })
