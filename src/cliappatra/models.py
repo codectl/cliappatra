7a533885e8bea865a0481a6742d003cbb39ffcd8
Renato Damas <me@renatodamas.com>
2023-01-15 10:23:44 +0000

    refactor: make meta data a field attribute #wip

--- src/cliappatra/models.py
+++ src/cliappatra/models.py
@@ -1,7 +1,14 @@
-import argparse
 import inspect
 from dataclasses import dataclass
+from enum import Enum
 from typing import Any, Optional, Union
+
+
+@dataclass
+class FieldMeta:
+    name: str
+    kind: Enum
+    annotation: Any = inspect.Parameter.empty
 
 
 @dataclass
@@ -14,22 +21,7 @@ class Field:
     metavar: Optional[str] = None
     const: Optional[str] = None
     help: Optional[str] = None
+    meta: Optional[FieldMeta] = None
 
     def asdict(self):
         return {k: v for k, v in self.__dict__.items() if v is not None}
-
-
-class ParamMeta:
-
-    def __init__(
-            self,
-            *,
-            name: str,
-            kind: int,
-            default: Any = inspect.Parameter.empty,
-            annotation: Any = inspect.Parameter.empty,
-    ) -> None:
-        self.name = name
-        self.kind = kind
-        self.default = default
-        self.annotation = annotation
