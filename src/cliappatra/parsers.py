6059f7fa483d8c59be0f8af9ef3c43973f6d1bd6
Renato Damas <me@renatodamas.com>
2023-01-17 14:20:26 +0000

    refactor: use staticmethods for parser #wip

--- src/cliappatra/parsers.py
+++ src/cliappatra/parsers.py
@@ -1,4 +1,5 @@
 import abc
+import argparse
 import inspect
 
 from cliappatra.models import Field
@@ -6,48 +7,43 @@ from cliappatra.models import Field
 
 class FieldParser:
 
-    def __init__(self, parser):
-        self._parser = parser
-
-    def parse(self, field: Field):
+    @staticmethod
+    def parse(field: Field):
         cls = _DefaultParser
         if field.meta.annotation is bool:
             cls = _BoolParser
-        return cls(self._parser).parse(field)
+        return cls.parse(field)
 
 
 class _BaseParser(abc.ABC):
-
-    def __init__(self, parser):
-        self._parser = parser
 
     @staticmethod
     @abc.abstractmethod
     def _parse_kwargs(kwargs: dict):
         pass
 
-    def parse(self, field: Field):
+    @classmethod
+    def parse(cls, field: Field):
         kwargs = field.asdict()
+        dest = field.meta.name
         meta = kwargs.pop("meta")
 
         if meta.kind == inspect.Parameter.POSITIONAL_ONLY:
-            name = meta.name
-            required = kwargs.pop("required")
+            required = kwargs.pop("required", True)
 
+            # enable optional arguments
             if "nargs" not in kwargs and not required:
-                kwargs["nargs"] = "?"
+                kwargs["nargs"] = argparse.OPTIONAL
 
             if "metavar" not in kwargs:
-                kwargs["metavar"] = name.upper()
+                kwargs["metavar"] = meta.name.upper()
 
         else:
-            name = f"--{meta.name}"
+            dest = f"--{dest}"
 
-        # required = kwargs.pop("required")
-        # env = kwargs.pop("envar")
-        kwargs = self._parse_kwargs(kwargs)
-
-        return self._parser.add_argument(name, **kwargs)
+        args = (dest,)
+        kwargs = cls._parse_kwargs(kwargs)
+        return args, kwargs
 
 
 class _DefaultParser(_BaseParser):
@@ -61,4 +57,12 @@ class _BoolParser(_BaseParser):
 
     @staticmethod
     def _parse_kwargs(kwargs: dict):
+        if "action" in kwargs:
+            pass
+        elif kwargs.get("required"):
+            kwargs["action"] = argparse.BooleanOptionalAction
+        elif kwargs.get("default"):
+            kwargs["action"] = "store_true"
+        else:
+            kwargs["action"] = "store_false"
         return kwargs
